255916bd500318b4ed40ce11db0503ac6a3729cd
nextmyth84-stack <nextmyth84@gmail.com>
2025-10-26 19:22:47 +0900

    Update app.py

--- app.py
+++ app.py
@@ -755,7 +755,7 @@ with tab2:
         run_a = st.button(
             "오전 GPT 인식",
             key="btn_a_ocr",
-            help="근무표에서 도로주행 근무자/제외자를 추출합니다."
+            help="근무표에서 도로주행 근무자를 추출합니다."
         )
     with col_desc:
         st.markdown(
