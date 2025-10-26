bc1dbfe97a98637941cf9822c64ead9fd989d08e
nextmyth84-stack <nextmyth84@gmail.com>
2025-10-26 18:38:52 +0900

    Update app.py

--- app.py
+++ app.py
@@ -346,13 +346,6 @@ div.stButton > button:hover { background-color: #1d4ed8; }
     color: #7c2d12;
     font-size: 13px;
     
-/* 라벨 크게 */
-.big-label { 
-  font-size: 22px;      /* ← 여기 숫자로 크기 조절 */
-  font-weight: 700;
-  color: #1f2937;       /* slate-800 */
-  margin: 6px 0 4px;
-}
 </style>
 """, unsafe_allow_html=True)
 
